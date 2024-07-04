d3b7fb4262c43f9b7727f86f5b9359ed72b078d2
Sebashinojosa <sebastian.hinojosa@espoch.edu.ec>
2024-07-04 16:09:59 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+digitos <-  function(num_3_dig){
+  N <- n3dig
+  if(N >= 100 & N < 1000){
+    dig1 <- trunc(N/100)
+    x1 <- N %% 100
+    dig2 <- trunc(x1/10)
+    dig3 <- x1 %% 10
+    s <- dig1+dig2+dig3
+    print('La suma de los digitos es: ')
+    print(s)
+  }else{
+    print('No es un numero de 3 digitos')
+  }
+}
